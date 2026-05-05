7e9d7e96e0695966e59d33c430f56f37c613a182
fathimath20250753-dotcom <fathimath20250753@miis.ae>
2026-05-05 11:42:41 +0400

    Update linear.py

--- linear.py
+++ linear.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
-df = pd.read_csv("data.csv")
+df = pd.read_csv("student_scores.csv")
 X = df[['HoursStudied']]
 y = df['ExamScore']
 model = LinearRegression()
